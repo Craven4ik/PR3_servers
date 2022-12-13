7c3cd88fea42d264c416d165fd4188f652ad3f4a
Max <Craven4ik@yandex.ru>
2022-12-13 17:52:53 +0300

    add practice 6

--- src/pr5/upload.php
+++ src/pr5/upload.php
@@ -2,6 +2,7 @@
 $fileName = $_FILES['uploadedFile']['name'];
 $fileNameCmps = explode(".", $fileName);
 $fileExtension = strtolower(end($fileNameCmps));
+finfo_file();
 
 $blob = addslashes(file_get_contents($_FILES['uploadedFile']['tmp_name']));
 $con = new mysqli('MYSQL', 'user', 'password', 'dataDB');
